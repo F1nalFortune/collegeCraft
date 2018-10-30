5146ca64ea2094781153cfabaed95a7a5d4c31fe
Brian <juniortoad1738@gmail.com>
2018-10-30 16:57:54 -0400

    stubbed out basic pages

---
+++ views/about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include './partials/head.html' ?>
+</head>
+<body>
+  <?php include './partials/header.html' ?>
+  <h1>About</h1>
+  <p>
+    Lorem Khaled Ipsum is a major key to success. Life is what you make it, so let’s make it. You smart, you loyal, you a genius. Surround yourself with angels, positive energy, beautiful people, beautiful souls, clean heart, angel. Every chance I get, I water the plants, Lion! The key to more success is to get a massage once a week, very important, major key, cloth talk. Look at the sunset, life is amazing, life is beautiful, life is what you make it. They never said winning was easy. Some people can’t handle success, I can.
+
+Eliptical talk. Every chance I get, I water the plants, Lion! They don’t want us to eat. Wraith talk. The first of the month is coming, we have to get money, we have no choice. It cost money to eat and they don’t want you to eat. Put it this way, it took me twenty five years to get these plants, twenty five years of blood sweat and tears, and I’m never giving up, I’m just getting started.
+  </p>
+</body>
+</html>
---
+++ views/dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include './partials/head.html' ?>
+</head>
+<body>
+  <?php include './partials/header.html' ?>
+  <h1>Dashboard</h1>
+  <p>
+    Lorem Khaled Ipsum is a major key to success. Celebrate success right, the only way, apple. I’m up to something. The key to more success is to get a massage once a week, very important, major key, cloth talk. Lion! Learning is cool, but knowing is better, and I know the key to success. It’s on you how you want to live your life. Everyone has a choice. I pick my choice, squeaky clean. A major key, never panic. Don’t panic, when it gets crazy and rough, don’t panic, stay calm. Let me be clear, you have to make it through the jungle to make it to paradise, that’s the key, Lion!
+
+    Celebrate success right, the only way, apple. Surround yourself with angels. Special cloth alert. The first of the month is coming, we have to get money, we have no choice. It cost money to eat and they don’t want you to eat. Congratulations, you played yourself. Major key, don’t fall for the trap, stay focused. It’s the ones closest to you that want to see you fail. Mogul talk. Find peace, life is like a water fall, you’ve gotta flow. We the best. The key is to drink coconut, fresh coconut, trust me. The weather is amazing, walk with me through the pathway of more success. Take this journey with me, Lion!
+  </p>
+</body>
+</html>
