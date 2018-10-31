e105959d8d0e51f4019eb62d8b0f0f60ca0807bb
Your Name <you@uconn.edu>
2018-10-31 13:22:53 -0400

    added a login and registration page.. these pages currently work with the university database and specifically a table in that database called users where the user and hashed(pwd) info is stored.. this can easily be changed to a new database and table just by refactoring

--- views/index.php
+++ views/index.php
@@ -1,11 +1,76 @@
 <?php
   include './partials/header.html';
  ?>
+
+<?php 
+
+session_start();
+
+?>
+
+<?php
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        $_SESSION["usr"] = $_POST['username'];
+        $_SESSION["pwd"] = $_POST['password'];
+        $pass = crypt($_SESSION["pwd"],'$1$salt012345'); #cmp with stored
+        $user = $_SESSION["usr"];
+        $conn = @new mysqli('127.0.0.1', 'root', '','university');
+        $sql = "select hashed_password from users where username='" . $user . "';";
+        $result = $conn->query($sql);
+        if ( $result->num_rows === 0 ){
+                echo "No Login Available";
+                header("Location: registration.php");
+        }
+        $_SESSION["result"] = $result;
+        $row = $result->fetch_row();
+        echo $row[0];
+        echo "1000000";
+	if($row[0] === $pass){
+                echo "success";
+                $_SESSION["loggedin"] = "1";
+                header("Location: dashboard.php");
+        }else{
+                echo "bad password";
+                header("Location: index.php");
+                echo "bad password";
+        }
+
+
+}else{
+
+session_unset();
+session_destroy();
+
+}
+
+?>
+
+
+
 <html>
   <head>
+    <title> Welcome to the College Craft </title>
     <?php include './partials/head.html' ?>
   </head>
   <body>
-    <p>Hello World!</p>
+    <div style="">
+    <form action="index.php" method="POST" style="position:relative;left:20px;">
+    <p>Log in here:
+    <br>
+    <span>Username: <input type="Text" name="username"/> </span>
+    <br>
+    <span>Password: <input type="password" name="password"/> </span>
+    </p>
+    <input type="Submit" value = "Submit"/>
+    <br>
+    <p>Not registered? Register <a href="registration.php"> here </a> </p>
+
+    </form>
+    </div>
+
+
+
   </body>
 </html>
---
+++ views/registration.php
@@ -0,0 +1,53 @@
+<html>
+
+<?php
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$conn = @new mysqli($servername, $username, $password,'university');
+	$userToAdd = $_POST['usr'];
+	$pwdToAdd = $_POST['pwd'];
+	$hashedPass = crypt($pwdToAdd,'$1$salt012345');
+	$sql = "INSERT INTO users (username, hashed_password) VALUES ('$userToAdd', '$hashedPass' );";
+	if($conn->query($sql) === TRUE){
+		echo "successfully added";
+		header("Location: index.php");
+	}else{
+		echo "error $conn->error()";
+	}
+	#$sql = "INSERT INTO users(username, password) VALUES ($_POST["usr"], crypt($_SESSION["pwd"],"$1$salt012345") );";	
+	}
+
+?>
+
+<head>
+
+<title> Registration Page </title>
+
+</head>
+
+<body>
+
+<div style="position:relative;left:200px;top:200px;border:solid;width:300px;">
+
+<form action="registration.php" method="POST" style="position:relative;left:20px;">
+	<p>Register Here:
+	<br>
+	<br />
+	<span>Username <input type="Text" name="usr"/> </span> 
+	<br>
+	<br />
+	<span>Password <input type="password" name="pwd"/> </span>
+	</p>
+	<input type="Submit" value = "Register"/>
+	<br />
+</form>
+
+</div>
+
+</body>
+
+</html>
