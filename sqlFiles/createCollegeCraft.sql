bdb429ab1ff0b211d235870749043cf259568e73
Brian Phelps <phelpsb92@gmail.com>
2018-11-09 15:02:04 -0500

    created database

--- sqlFiles/createCollegeCraft.sql
+++ sqlFiles/createCollegeCraft.sql
@@ -1,14 +1,65 @@
 DROP DATABASE IF EXISTS collegeCraft;
 CREATE DATABASE IF NOT EXISTS collegeCraft;
 USE collegeCraft;
-DROP TABLE IF EXISTS users, roles, product, comments, sells, reputation;
-CREATE TABLE users (username varchar(15) NOT NULL, hashed_password varchar(255), email_address varchar(30), user_id INT, bio varchar(512), reputation_id INT, role_id INT, location varchar(128), name varchar(32), PRIMARY KEY (username));
-CREATE TABLE roles (role_id INT, role varchar(20), PRIMARY KEY (role_id));
-CREATE TABLE reputation (reputation_id INT, trust_level INT, statistics varchar(256), PRIMARY KEY (reputation_id));
-CREATE TABLE sells (user_id INT, product_id INT);
-CREATE TABLE product (product_id INT, description varchar(256), price INT, category varchar(256), PRIMARY KEY (product_id));
-CREATE TABLE comments (comment_id INT, product_id INT, user_id INT, review varchar(256), rating INT, PRIMARY KEY (comment_id));
+DROP TABLE IF EXISTS users, roles, product, comments, sells, reputation, trade_ad;
+CREATE TABLE users (
+  user_id INT AUTO_INCREMENT,
+  username varchar(15) NOT NULL,
+  hashed_password varchar(255),
+  email_address varchar(30),
+  bio varchar(512),
+  reputation_id INT,
+  role_id INT,
+  location varchar(128),
+  name varchar(32),
+  PRIMARY KEY (user_id)
+);
+CREATE TABLE roles (
+  role_id INT AUTO_INCREMENT,
+  role varchar(20),
+  PRIMARY KEY (role_id)
+);
+CREATE TABLE reputation (
+  reputation_id INT AUTO_INCREMENT,
+  trust_level INT,
+  statistics varchar(256),
+  PRIMARY KEY (reputation_id)
+);
+CREATE TABLE sells (
+  user_id INT,
+  trade_ad_id INT,
+  CONSTRAINT SELLS_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+  CONSTRAINT SELLS_trade FOREIGN KEY (trade_ad_id) REFERENCES trade_ad(id),
+);
+CREATE TABLE product (
+  product_id INT AUTO_INCREMENT,
+  description varchar(256),
+  category varchar(256),
+  PRIMARY KEY (product_id)
+);
+CREATE TABLE comments (
+    comment_id INT AUTO_INCREMENT,
+    trade_ad_id INT,
+    user_id int,
+    review varchar(256),
+    rating INT,
+    PRIMARY KEY (comment_id),
+    CONSTRAINT COMMENTS_tradeid FOREIGN KEY (trade_ad_id) REFERENCES trade_ad(id),
+    CONSTRAINT COMMENTS_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+create table trade_ad(
+    id int not null AUTO_INCREMENT,
+    product_id int,
+    seller varchar(15),
+    buyer varchar(15),
+    price float,
+    qty int,
+    PRIMARY KEY (id)
+);
 INSERT INTO users (username, hashed_password) VALUES ('aaaa','$1$salt0123$DDmlS/eUhEk23hF59K/Yv/');
 INSERT INTO users (username, hashed_password) VALUES ('aaaaaa','$1$salt0123$01QCUmrv4.Ao3hQdGHB6c1');
 INSERT INTO users (username, hashed_password) VALUES ('hello','$1$salt0123$3vhIeCFYVtADi.UiQs3Ae/');
 INSERT INTO users (username, hashed_password) VALUES ('zebra','$1$salt0123$N8X1JXgNGT5iFOZslL0N90');
+INSERT INTO users (username, hashed_password) VALUES ('admin','$1$salt0123$O7peeN/6eCXL0x9F/yb.81');
+INSERT INTO users (username, hashed_password) VALUES ('admin','$1$salt0123$O7peeN/6eCXL0x9F/yb.81');
+INSERT INTO users (username, hashed_password) VALUES ('admin','$1$salt0123$O7peeN/6eCXL0x9F/yb.81');
---
+++ sqlFiles/createDatabase.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2018 at 08:42 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+DROP DATABASE IF EXISTS collegeCraft;
+CREATE DATABASE IF NOT EXISTS collegeCraft;
+USE collegeCraft;
+DROP TABLE IF EXISTS users, roles, product, comments, sells, reputation, trade_ad;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `collegecraft`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `trade_ad_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `review` varchar(256) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  `category` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reputation`
+--
+
+CREATE TABLE `reputation` (
+  `id` int(11) NOT NULL,
+  `trust_level` int(11) DEFAULT NULL,
+  `statistics` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `role_id` int(11) NOT NULL,
+  `role` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sells`
+--
+
+CREATE TABLE `sells` (
+  `user_id` varchar(64) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trade_ad`
+--
+
+CREATE TABLE `trade_ad` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `seller` int(11) DEFAULT NULL,
+  `buyer` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `hashed_password` varchar(255) DEFAULT NULL,
+  `email_address` varchar(30) DEFAULT NULL,
+  `bio` varchar(512) DEFAULT NULL,
+  `reputation_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `location` varchar(128) DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `COMMENTS_tradeid` (`trade_ad_id`),
+  ADD KEY `COMMENTS_user` (`user_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `reputation`
+--
+ALTER TABLE `reputation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `trade_ad`
+--
+ALTER TABLE `trade_ad`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `TRADE_Product` (`product_id`),
+  ADD KEY `TRADE_Seller` (`seller`),
+  ADD KEY `TRADE_Buyer` (`buyer`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reputation`
+--
+ALTER TABLE `reputation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `trade_ad`
+--
+ALTER TABLE `trade_ad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `COMMENTS_tradeid` FOREIGN KEY (`trade_ad_id`) REFERENCES `trade_ad` (`id`),
+  ADD CONSTRAINT `COMMENTS_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+
+  ALTER TABLE `users`
+    ADD CONSTRAINT `USERS` FOREIGN KEY (`reputation_id`) REFERENCES `reputation` (`id`);
+
+--
+-- Constraints for table `trade_ad`
+--
+ALTER TABLE `trade_ad`
+  ADD CONSTRAINT `TRADE_Buyer` FOREIGN KEY (`buyer`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `TRADE_Product` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`),
+  ADD CONSTRAINT `TRADE_Seller` FOREIGN KEY (`seller`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
