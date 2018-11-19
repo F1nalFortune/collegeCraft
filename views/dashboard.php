bd2aa095bbd6c3e4dd0751cc05851765ca99d8bd
Brian <juniortoad1738@gmail.com>
2018-11-19 16:40:25 -0500

    pagination

--- views/dashboard.php
+++ views/dashboard.php
@@ -3,7 +3,7 @@
 	session_start();
 
 	include '../connect.php';
-
+	$output = '';
 	function fill_university($conn){
 		$tableJoin = "SELECT DISTINCT location FROM (
 			SELECT Users.location, Trade_ad.id as Trade_ID, Trade_ad.product_id, Trade_ad.price, Product.name
@@ -33,7 +33,34 @@
 
 		return $output;
 	}
-	//
+
+	if (isset($_GET['pageno'])) {
+			$pageno = $_GET['pageno'];
+	} else {
+			$pageno = 1;
+	}
+
+	$no_of_records_per_page = 12;
+	$offset = ($pageno-1) * $no_of_records_per_page;
+
+	include '../connect.php';
+
+	$total_pages_sql = "SELECT COUNT(*) FROM trade_ad";
+	$result = mysqli_query($conn,$total_pages_sql);
+	$total_rows = mysqli_fetch_array($result)[0];
+	$total_pages = ceil($total_rows / $no_of_records_per_page);
+
+	$sql = "SELECT * FROM ( SELECT Users.location, Trade_ad.id as Trade_ID, Trade_ad.product_id, Trade_ad.price, Product.name FROM Users INNER JOIN Trade_ad ON Users.user_id=Trade_ad.seller INNER JOIN Product ON Trade_ad.product_id=Product.product_id ) as Full_Ad LIMIT $offset, $no_of_records_per_page";
+	$res_data = mysqli_query($conn,$sql);
+	while($row = mysqli_fetch_array($res_data)){
+			$output .= "<div class='col-sm-4' style='border: 1px solid black'>
+	                  <div><a href='item.php?item={$row['Trade_ID']}'>{$row['name']}</a></div>
+	              </div>";
+	}
+
+
+
+//
 	// $conn->query("create table if not exists product(
 	// 	product_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	// 	description varchar(255) NOT NULL,
@@ -77,20 +104,48 @@
 		</div>
 		<div class='col-sm-9'>
 			<div class='row' id="show_ad">
-				<?php echo fill_ad($conn); ?>
+				<?php echo $output ?>
 			</div>
+			<nav>
+				<ul class='pagination justify-content-center' style='padding-top: 5%;'>
+						<li><a href='?pageno=1'><i class="fa fa-fast-backward" aria-hidden="true"></i></a></li>
+						<li class='<?php if($pageno <= 1){ echo 'disabled'; } ?> page-item'>
+								<a href='<?php if($pageno <= 1){ echo '#'; } else { echo '?pageno='.($pageno - 1); } ?>'>
+									<i class="fas fa-caret-left  fa-2x" style='padding-right: 15px;padding-left: 15px;'></i>
+								</a>
+						</li>
+						<li class='<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>'>
+								<a href='<?php if($pageno >= $total_pages){ echo '#'; } else { echo '?pageno='.($pageno + 1); } ?> '>
+									<i class="fas fa-caret-right fa-2x" style='padding-right: 15px;padding-left: 15px;'></i>
+								</a>
+						</li>
+						<li><a href='?pageno=<?php echo $total_pages; ?>'><i class="fa fa-fast-forward" aria-hidden="true"></i></a></li>
+				</ul>
+			</nav>
 		</div>
 	</div>
-	<nav aria-label='Page navigation example'>
-	  <ul class='pagination'>
-	    <li class='page-item'><a class='page-link' href='#'>Previous</a></li>
-	    <li class='page-item'><a class='page-link' href='#'>1</a></li>
-	    <li class='page-item'><a class='page-link' href='#'>2</a></li>
-	    <li class='page-item'><a class='page-link' href='#'>3</a></li>
-	    <li class='page-item'><a class='page-link' href='#'>Next</a></li>
+
+<!--
+	<nav aria-label="Page navigation example">
+	  <ul class="pagination">
+	    <li class="page-item">
+	      <a class="page-link" href="#" aria-label="Previous">
+
+	      </a>
+	    </li>
+	    <li class="page-item"><a class="page-link" href="#">1</a></li>
+	    <li class="page-item"><a class="page-link" href="#">2</a></li>
+	    <li class="page-item"><a class="page-link" href="#">3</a></li>
+	    <li class="page-item">
+	      <a class="page-link" href="#" aria-label="Next">
+	        <span aria-hidden="true">&raquo;</span>
+	        <span class="sr-only">Next</span>
+	      </a>
+	    </li>
 	  </ul>
-	</nav>
+	</nav> -->
 </div>
+
 
 </body>
 <script>
@@ -120,7 +175,7 @@ $(document).ready(function(){
 			}
 		})
 	});
-	
+
 
 	$('#search').change(function(){
 		var search = $("#search").val();
---
+++ views/test.php
@@ -0,0 +1,46 @@
+
+<html>
+<head>
+    <title>Pagination</title>
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <?php
+
+        if (isset($_GET['pageno'])) {
+            $pageno = $_GET['pageno'];
+        } else {
+            $pageno = 1;
+        }
+        $no_of_records_per_page = 10;
+        $offset = ($pageno-1) * $no_of_records_per_page;
+
+        include '../connect.php';
+
+        $total_pages_sql = "SELECT COUNT(*) FROM trade_ad";
+        $result = mysqli_query($conn,$total_pages_sql);
+        $total_rows = mysqli_fetch_array($result)[0];
+        $total_pages = ceil($total_rows / $no_of_records_per_page);
+
+        $sql = "SELECT * FROM trade_ad LIMIT $offset, $no_of_records_per_page";
+        $res_data = mysqli_query($conn,$sql);
+        while($row = mysqli_fetch_array($res_data)){
+            echo $row['product_id'];
+        }
+        mysqli_close($conn);
+    ?>
+    <ul class="pagination">
+        <li><a href="?pageno=1">First</a></li>
+        <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
+            <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>">Prev</a>
+        </li>
+        <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
+            <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
+        </li>
+        <li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+    </ul>
+</body>
+</html>
