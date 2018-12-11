2be6ab4b7155064239a77c05a5cb3a19f7011d85
Brian Phelps <phelpsb92@gmail.com>
2018-12-10 23:24:37 -0500

    fixed sort_feedback

--- sort_feedback.php
+++ sort_feedback.php
@@ -17,16 +17,17 @@ if(isset($_POST['sort'])){
     //         WHERE seller = {$user}
     //         ORDER BY created DESC";
             // UPDATED QUERY
-            $sql = "SELECT review.id, review.review, review.trade_id, review.feedback,
-                      review.created, trade_ad.id, trade_ad.price, trade_ad.qty,
-                      product.name, users.username, sells.user_id as seller, buys.user_id as buyer
-                    FROM review
-                    INNER JOIN Trade_ad ON review.trade_id=Trade_ad.id
-                    INNER JOIN sells on trade_ad.id=sells.product_id
-                    INNER JOIN buys on trade_ad.id=buys.product_id
-                    INNER JOIN product ON trade_ad.id=product.product_id
-                    INNER JOIN users ON sells.user_id=users.user_id
-                    WHERE sells.user_id = {$user}
+            $sql = "SELECT review.id, review.review, review.trade_id,
+              review.feedback, review.created, buys.user_id as buyer, advertises.product_id,
+              trade_ad.price, trade_ad.qty, sells.user_id as seller, product.name, users.username
+            FROM review
+            INNER JOIN Trade_ad ON review.trade_id=Trade_ad.id
+            INNER JOIN sells on trade_ad.id=sells.product_id
+            INNER JOIN product ON trade_ad.id=product.product_id
+            INNER JOIN users ON users.user_id=sells.user_id
+            INNER JOIN buys on trade_ad.id=buys.product_id
+            INNER JOIN advertises on trade_ad.id = advertises.trade_ad_id
+            WHERE sells.user_id = {$user}
                     ORDER BY created DESC";
   } else if ($sort=='oldest'){
     // $sql = "SELECT review.id, review.review, review.trade_id, review.feedback,
@@ -39,16 +40,17 @@ if(isset($_POST['sort'])){
     //         WHERE seller = {$user}
     //         ORDER BY created ASC";
             // UPDATED QUERY
-            $sql = "SELECT review.id, review.review, review.trade_id, review.feedback,
-                      review.created, trade_ad.id, trade_ad.price, trade_ad.qty,
-                      product.name, users.username, sells.user_id as seller, buys.user_id as buyer
-                    FROM review
-                    INNER JOIN Trade_ad ON review.trade_id=Trade_ad.id
-                    INNER JOIN sells on trade_ad.id=sells.product_id
-                    INNER JOIN buys on trade_ad.id=buys.product_id
-                    INNER JOIN product ON trade_ad.id=product.product_id
-                    INNER JOIN users ON users.user_id=sells.user_id
-                    WHERE sells.user_id = {$user}
+            $sql = "SELECT review.id, review.review, review.trade_id,
+              review.feedback, review.created, buys.user_id as buyer, advertises.product_id,
+              trade_ad.price, trade_ad.qty, sells.user_id as seller, product.name, users.username
+            FROM review
+            INNER JOIN Trade_ad ON review.trade_id=Trade_ad.id
+            INNER JOIN sells on trade_ad.id=sells.product_id
+            INNER JOIN product ON trade_ad.id=product.product_id
+            INNER JOIN users ON users.user_id=sells.user_id
+            INNER JOIN buys on trade_ad.id=buys.product_id
+            INNER JOIN advertises on trade_ad.id = advertises.trade_ad_id
+            WHERE sells.user_id = {$user}
                     ORDER BY created ASC";
   } else if ($sort=='best'){
     // $sql = "SELECT review.id, review.review, review.trade_id, review.feedback,
@@ -61,16 +63,17 @@ if(isset($_POST['sort'])){
     //         WHERE seller = {$user}
     //         ORDER BY review DESC";
             //UPDATED QUERY
-            $sql = "SELECT review.id, review.review, review.trade_id, review.feedback,
-                      review.created, trade_ad.id, trade_ad.price, trade_ad.qty,
-                      product.name, users.username, sells.user_id as seller, buys.user_id as buyer
-                    FROM review
-                    INNER JOIN Trade_ad ON review.trade_id=Trade_ad.id
-                    INNER JOIN sells on trade_ad.id=sells.product_id
-                    INNER JOIN buys on trade_ad.id=buys.product_id
-                    INNER JOIN product ON trade_ad.id=product.product_id
-                    INNER JOIN users ON users.user_id=sells.user_id
-                    WHERE sells.user_id = {$user}
+            $sql = "SELECT review.id, review.review, review.trade_id,
+              review.feedback, review.created, buys.user_id as buyer, advertises.product_id,
+              trade_ad.price, trade_ad.qty, sells.user_id as seller, product.name, users.username
+            FROM review
+            INNER JOIN Trade_ad ON review.trade_id=Trade_ad.id
+            INNER JOIN sells on trade_ad.id=sells.product_id
+            INNER JOIN product ON trade_ad.id=product.product_id
+            INNER JOIN users ON users.user_id=sells.user_id
+            INNER JOIN buys on trade_ad.id=buys.product_id
+            INNER JOIN advertises on trade_ad.id = advertises.trade_ad_id
+            WHERE sells.user_id = {$user}
                     ORDER BY review DESC";
   } else {
     console.log('fail');
